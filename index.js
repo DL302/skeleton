9ce8c4e0176533c569e180087a3e27ddd8d74378
DL302 <32117423+DL302@users.noreply.github.com>
2020-12-28 17:52:05 -0800

    added ping command

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const config = require('./config.json');
 const command = require('./command');
 client.on('ready', () => {
     console.log('start.');
+
+    command(client, 'ping', (message) => {
+        message.channel.send('Pong.');
+    });
 });
 
 client.login(config.token);

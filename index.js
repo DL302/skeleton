60547df0f3b1b31bb2898c69b70b37a2c8420602
DL302 <32117423+DL302@users.noreply.github.com>
2020-12-28 20:09:26 -0800

    converted shrugflip command to a slash command

--- index.js
+++ index.js
@@ -10,8 +10,38 @@ client.on('ready', () => {
         message.channel.send('Pong.');
     });
 
+    /*
+    //regular shrugflip command
     command(client, ['shrugflip', 'sf'], (message) => {
         message.channel.send('┻━┻︵ ¯\\_(ツ)_/¯ ︵ ┻━┻');
+    });
+    */
+
+    // sf/shrugflip command using new slash command feature
+    // client.api.applications(client.user.id).guilds('765292898715303936').commands.post({
+    client.api.applications(client.user.id).commands.post({
+        data: {
+            name: 'shrugflip',
+            description: 'shrug + tableflip'
+            // possible options here e.g. options: [{...}]
+        }
+    });
+
+
+    client.ws.on('INTERACTION_CREATE', async interaction => {
+        const command = interaction.data.name.toLowerCase();
+        const args = interaction.data.options;
+
+        if (command === 'shrugflip') {
+            client.api.interactions(interaction.id, interaction.token).callback.post({
+                data: {
+                    type: 4,
+                    data: {
+                        content: '┻━┻︵ ¯\\_(ツ)_/¯ ︵ ┻━┻'
+                    }
+                }
+            });
+        }
     });
 });
 

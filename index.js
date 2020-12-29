1623c3e5d4a94d7d40cdc9d2caa4f9ad8f2955b7
DL302 <32117423+DL302@users.noreply.github.com>
2020-12-28 17:44:04 -0800

    added command functionality

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const config = require('./config.json');
 
+const config = require('./config.json');
+const command = require('./command');
 client.on('ready', () => {
     console.log('start.');
 });

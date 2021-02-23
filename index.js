39aae09f83a5b16876f5c7f4cddd5b5a2eef80d2
DL302 <32117423+DL302@users.noreply.github.com>
2021-02-22 22:14:48 -0800

    added 8ball command

--- index.js
+++ index.js
@@ -14,6 +14,72 @@ client.on('ready', () => {
         message.channel.send('https://raw.githubusercontent.com/DL302/skeleton/master/assets/a2a.png');
     });
 
+    command(client, '8ball', message => {
+        let response = '';
+        switch (Math.floor(Math.random() * 20)) {
+            case 0:
+                response = 'It is certain.';
+                break;
+            case 1:
+                response = 'It is decidedly so.';
+                break;
+            case 2:
+                response = 'Without a doubt.';
+                break;
+            case 3:
+                response = 'Yes - definitely.';
+                break;
+            case 4:
+                response = 'You may rely on it.';
+                break;
+            case 5:
+                response = 'As I see it, yes.';
+                break;
+            case 6:
+                response = 'Most likely.';
+                break;
+            case 7:
+                response = 'Outlook good.';
+                break;
+            case 8:
+                response = 'Yes.';
+                break;
+            case 9:
+                response = 'Signs point to yes.';
+                break;
+            case 10:
+                response = 'Reply hazy, try again.';
+                break;
+            case 11:
+                response = 'Ask again later.';
+                break;
+            case 12:
+                response = 'Better not tell you now.';
+                break;
+            case 13:
+                response = 'Cannot predict now.';
+                break;
+            case 14:
+                response = 'Concentrate and ask again.';
+                break;
+            case 15:
+                response = 'Don\'t count on it.';
+                break;
+            case 16:
+                response = 'My reply is no.';
+                break;
+            case 17:
+                response = 'My sources say no.';
+                break;
+            case 18:
+                response = 'Outlook not so good.';
+                break;
+            case 19:
+                response = 'Very doubtful.';
+                break;
+        }
+        message.channel.send(response);
+    });
     command(client, 'eval', message => {
         const { member, channel, content } = message;
         if (member.id === config.ownerID && content.search(/config/gi) === -1) {
